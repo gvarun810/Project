04a40928647ac161dd361cb55a4079e04c6518d8
gvarun810 <68148369+gvarun810@users.noreply.github.com>
2022-04-12 20:11:05 +0530

    Updated Project

---
+++ FileFinder.py
@@ -0,0 +1,76 @@
+from itertools import chain
+import os
+import sys
+
+print("Do you want to search file.")
+print ("1. word list in the dictory")
+print ("2. On the basis extension")
+A = float(input("Enter the choice: "))
+Document = ('.docx','.pdf','doc','csv','.xlsx','.xlsm','.xlsb','.xltx','.xltm','.xls','.xlt','.xls','.xml','.xlam')
+Image = ('.apng','.avif','.gif','.jpg','.jpeg','.jfif','.pjpeg','.pjp','.png','.svg','.webp','.bmp','.ico','.cur','.tif','.tiff','.heic')
+Video = ('.mp4','.mov','.wmv','.avi','.avchd','.flv','.f4v','.swf','.mkv')
+Rape = ('Child abuse','Domestic violence','Blackmail','Guilt','Sex','Murder','Victim','Fear','Gang Rape','Rod','Vagina','Condoms')
+Murder = ('Arson','Hit and run','Homicide','Poaching','Genocide','Assassination','Felony','Manslaughter','Bloodshed','Guns','Pistols','Rifle','Knife','Hammer','Strangulation')
+Terrorism = ('Bombing','Espionage','Dynamite','Conflict','Hijacking','Kidnapping','Cyber terrorism','Explosive','Explosion')
+paths = ('C:','D:')
+
+if (A == 1):
+    print("Which Case")
+    print("1.Murder")
+    print("2.Rape")
+    print("3.Terrorism")
+    Madhu = float(input("Enter Your Choice: "))
+    Z = open('D:\Outputs\Murder\output.txt','w+')
+    sys.stdout = Z
+    if (Madhu == 1):
+        for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
+            for file in files:
+                if file.startswith(Murder):
+                    print(os.path.join(root, file))
+    Z.close()
+    N = open('D:\Outputs\Rape\output.txt','w+')
+    sys.stdout = N
+    if (Madhu == 2):
+        for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
+            for file in files:
+                if file.startswith(Rape):
+                    print(os.path.join(root, file))
+    N.close()
+    I = open('D:\Outputs\Terrorism\output.txt','w+')
+    sys.stdout = I
+    if (Madhu == 3):
+        for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
+            for file in files:
+                if file.startswith(Terrorism):
+                    print(os.path.join(root, file))
+    I.close()
+if (A==2):
+    print("Which File type you want to search")
+    print("1.Picture")
+    print("2.Document")
+    print("3.Video")
+    D = float(input("Enter Choice: "))
+    L = open('D:\Outputs\Picture\output.txt','w+')
+    sys.stdout = L
+    if (D == 1):
+        for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
+            for file in files:
+                if file.endswith(Image):
+                    print(os.path.join(root, file))
+    L.close()
+    M = open('D:\Outputs\Document\output.txt','w+')
+    sys.stdout = M
+    if (D == 2):
+        for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
+            for file in files:
+                if file.endswith(Document):
+                    print(os.path.join(root, file))
+    M.close()
+    Q = open('D:\Outputs\Video\output.txt','w+')
+    sys.stdout = Q
+    if (D==3):
+        for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
+            for file in files:
+                if file.endswith(Video):
+                    print(os.path.join(root, file))
+    Q.close()
